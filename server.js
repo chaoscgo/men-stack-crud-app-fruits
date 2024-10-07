2515f060753f051fd2d7f9cf29eccc1e6775261a
Beverly <chaoscgo@hotmail.com>
2024-10-07 14:50:49 -0500

    added Create a New Fruit

--- server.js
+++ server.js
@@ -12,11 +12,32 @@ mongoose.connect(process.env.MONGODB_URI);
 // Let's update the terminal with the connection status
 mongoose.connection.on('connected', () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`);
-})
+});
 
+const Fruit = require('./models/fruit');
+
+//Let's add our middleware
+app.use(express.urlencoded({ extended: false}));
+
+//  All of our routes
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/new.ejs');
+})
+
+app.post('/fruits', async (req, res) => {
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+
+    await Fruit.create(req.body);
+    res.redirect('/fruits/new');
 });
 
 app.listen(3000, () => {
