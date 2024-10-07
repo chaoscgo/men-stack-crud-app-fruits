e6ff152d8cdb62610225e095585367df09e9b70d
Beverly <chaoscgo@hotmail.com>
2024-10-07 13:57:02 -0500

    connect to MONGODB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Let's connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+// Let's update the terminal with the connection status
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+})
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
